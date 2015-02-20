2f87bf791289711e02697cc4e0bfc5bd7e9fca67
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:15:27 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,14 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-breadcrumb',
-  summary : 'Semantic UI - Breadcrumb (official): Single component release of breadcrumb',
-  version : '1.9.2',
+  summary : 'Semantic UI - Breadcrumb: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Breadcrumb.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'breadcrumb.css'
+  ], 'client');
 });
