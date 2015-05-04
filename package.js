fb12e00f400dcccb3c04603ab8852cab6512325f
Pusher Robot <jack@semantic-ui.com>
2015-05-04 12:58:21 -0400

    Updated component to version 1.12.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-breadcrumb',
   summary : 'Semantic UI - Breadcrumb: Single component release',
-  version : '1.12.1',
+  version : '1.12.2',
   git     : 'git://github.com/Semantic-Org/UI-Breadcrumb.git',
 });
 
